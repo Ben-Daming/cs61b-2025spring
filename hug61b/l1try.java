b85c62193bf953a1a51414d0f1154dbb8c0d13e9
Youdu Liu <liuyoudu24@mails.ucas.ac.cn>
2025-02-23 17:03:56 +0800

    first commit

---
+++ hug61b/l1try.java
@@ -0,0 +1,8 @@
+public class l1try{
+    public static void main(String[] args){
+        System.out.println("10 is string-type");
+        System.out.println(5+"10");
+        System.out.println("10 is integer-type");
+        System.out.println(5+10);
+    }
+}
